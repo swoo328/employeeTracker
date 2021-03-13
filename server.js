d6f78f953dc8ba57974d016f56bf3f927c51ee3c
Steven Woo <swoo9875@gmail.com>
2021-03-13 17:23:02 -0500

    add department and role to the table

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = 3000;
 
 const app = express();
 
+//connection to the server
 const connection = mysql.createConnection({
     "host": "localhost",
     "user": "swoo328",
@@ -18,10 +19,13 @@ app.get("/", (req, res) => {
     res.send("word");
 });
 
+
 connection.connect(() => {
     console.log("Connected");
     startQuestions();
 });
+
+//starting questions
 const startQuestions = () => {
     inquirer
         .prompt({
@@ -31,7 +35,12 @@ const startQuestions = () => {
             choices: [
                 "View all the departments",
                 "View all the roles",
-                "View all the employees"
+                "View all the employees",
+                "Add an employee",
+                "Add a role",
+                "Add a department",
+                "Update employee role",
+                "Exit"
             ]
         }).then(answer => {
             switch (answer.choice) {
@@ -44,33 +53,130 @@ const startQuestions = () => {
                 case "View all the employees":
                     viewEmployees();
                     break;
+                case "Add an employee":
+                    addEmployee();
+                    break;
+                case "Add a role":
+                    addRole();
+                    break;
+                case "Add a department":
+                    addDepartment();
+                    break;
+                case "Update employee role":
+                    updateEmployee();
                 default:
                     connection.end();
+                    console.log("Have a Nice Day");
                     break;
             }
         });
 }
+//view the employees in the table
 const viewEmployees = () => {
     connection.query("SELECT * FROM employee", function (err, data) {
         if (err) throw err;
+        //displays the table
         console.table(data);
         startQuestions();
     });
 };
+//view the departments in the table
 const viewDepartments = () => {
     connection.query("SELECT * FROM department", function (err, data) {
         if (err) throw err;
+        //displays the table
         console.table(data);
         startQuestions();
     });
 };
+//view the roles in the table
 const viewRoles = () => {
+    //(err,res) also works
     connection.query("SELECT * FROM role", function (err, data) {
         if (err) throw err;
+        //displays the table
         console.table(data);
         startQuestions();
     });
 };
+//adding a role to the table
+const addRole = () => {
+    inquirer
+        .prompt([
+            {
+                name: "role",
+                type: "input",
+                message: "Enter the role name:"
+            },
+            {
+                name: "roleSalary",
+                type: "input",
+                message: "What is the salary for the new role?"
+            }
+
+        ]).then(res => {
+            let role = res.role;
+            let roleSalary = res.roleSalary;
+            // let department = res.department;
+            connection.query("SELECT * FROM department", async (err, res) => {
+                if (err) throw err;
+                const departmentChoices = res.map(({ id, name }) => ({
+                    name: name,
+                    value: id
+                }))
+                console.log(departmentChoices);
+                const addDept = await inquirer.prompt([
+                    {
+                        name: "department",
+                        type: "list",
+                        message: "Which department would like to add to the role?",
+                        choices: departmentChoices
+                    }
+                ])
+                connection.query("INSERT INTO role SET?", {
+                    title: role,
+                    salary: roleSalary,
+                    department_id: addDept.department
+                }, function (err, res) {
+                    if (err) throw err;
+                    console.log("Role is added");
+                    startQuestions();
+                });
+            });
+
+        });
+
+};
+//adding a department to the table
+const addDepartment = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "department",
+            message: "Enter Department Name"
+        },
+    ]).then((res) => {
+        let department = res.department;
+        connection.query(
+            "INSERT INTO department SET ?",
+            {
+                name: department
+            },
+            function (err, res) {
+                if (err) throw err;
+                console.log("successfully added Department");
+                startQuestions();
+            });
+    });
+};
+//add an employee to the table
+const addEmployee = () => {
+
+};
+
+const updateEmployee = () => {
+
+}
 //data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
